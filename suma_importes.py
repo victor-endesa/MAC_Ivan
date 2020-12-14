6a46e86820428cc057d2c9fba0e7dd721e79e23f
victor-endesa <75997103+victor-endesa@users.noreply.github.com>
2020-12-14 16:23:36 +0100

    Update suma_importes.py

--- suma_importes.py
+++ suma_importes.py
@@ -1,3 +1,3 @@
-def calcular_suma_importes(importe_1, importe_2, importe_3):
+def calcular_suma_importes(importe_1, importe_2):
     
-    return importe_1+importe_2+importe_3
+    return importe_1+importe_2
